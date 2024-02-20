eae97d6683c3512e396b2b6a5976371f7a6e1189
Viktoria Yakush <viktoria.yakush@aliexpress.ru>
2024-02-20 15:33:49 +0300

    add pipeline actions

--- config/.storybook/main.ts
+++ config/.storybook/main.ts
@@ -10,7 +10,6 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "storybook-addon-react-router-v6",
   ],
   framework: {
     name: "@storybook/react-webpack5",
