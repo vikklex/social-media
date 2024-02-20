6dd5d8fb4bf83adea74ce78dbcc1195d8d5d4f57
Viktoria Yakush <viktoria.yakush@aliexpress.ru>
2024-02-20 17:02:07 +0300

    add pipeline actions

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,6 @@
 import webpack from "webpack";
 import path from "path";
-import buildWebpackConfig from "./config/build/buildWebpackConfig";
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
 import { BuildEnv, BuildPaths } from "./config/build/types/config";
 
 export default (env: BuildEnv) => {
